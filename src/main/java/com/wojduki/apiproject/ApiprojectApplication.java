128467c6dcdeee60ccfebaf2a16a74566dd7c752
wojduki <wojduki@gmail.com>
2024-09-09 21:34:25 +0200

    application file added

---
+++ src/main/java/com/wojduki/apiproject/ApiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.wojduki.apiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiprojectApplication.class, args);
+	}
+
+}
